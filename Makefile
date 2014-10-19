884b3bf974a2e22331844cd8c820f2486db8456c
David Capwell <dcapwell@davidcapwellmbp.attlocal.net>
2014-10-18 20:13:31 -0700

    Switched to 64bit by default.  Fixed compile bug with c++11

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SHELL:=/bin/bash
 UNAME:=$(shell uname | tr '[A-Z]' '[a-z]')
 
-BITS?=32
+BITS?=64
 ifeq ($(UNAME), darwin)
   READLINK_ARGS:=""
   PLATFORM_WARNINGS:=-Weverything -Wno-c++98-compat-pedantic -Wno-padded \
--- src/display.cc
+++ src/display.cc
@@ -20,7 +20,7 @@ void StackTracesPrinter::PrintStackTraces(TraceData *traces, int length) {
     if (traces[i].count != 0) {
       total += traces[i].count;
       count++;
-      fprintf(file_, "%"PRIdPTR" ", traces[i].count);
+      fprintf(file_, "%" PRIdPTR " ", traces[i].count);
       PrintStackTrace(&traces[i]);
       fprintf(file_, "\n");
     }
